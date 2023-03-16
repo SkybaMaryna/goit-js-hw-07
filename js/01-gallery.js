0a7c8f9f03ab98e7f9dedeb28130ff2a102daef5
SkybaMaryna <marinkaskiba@gmail.com>
2023-03-16 14:08:48 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,57 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+    galleryEl: document.querySelector('.gallery')
+}
+
+const makeCard = ({ preview, original, description }) => {
+    const itemEl = document.createElement('div')
+    itemEl.classList.add('gallery__item')
+
+    const linkEl = document.createElement('a')
+    linkEl.classList.add('gallery__link')
+    linkEl.setAttribute('href', original )
+
+    const imageEl = document.createElement('img')
+    imageEl.classList.add('gallery__image')
+    imageEl.setAttribute('src', preview)
+    imageEl.setAttribute('data-source', original)
+    imageEl.setAttribute('alt', description)
+
+    linkEl.append(imageEl)
+    itemEl.append(linkEl)
+
+    return itemEl
+}
+
+const cards = galleryItems.map(makeCard)
+
+refs.galleryEl.append(...cards)
+
+refs.galleryEl.addEventListener('click', onGetLink)
+
+function onGetLink(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+    return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+instance.show()
+
+   if (basicLightbox.visible()) {
+    refs.galleryEl.addEventListener('keydown', onClickEscape)
+   }
+
+    function onClickEscape(event) {
+        if (event.code === "Escape") {
+            console.log(event.code);
+            instance.close()
+            refs.galleryEl.removeEventListener('keydown', onClickEscape)
+        }
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+    galleryEl: document.querySelector('.gallery')
+}
+
+const makeCard = ({ preview, original, description }) => {
+    const itemEl = document.createElement('a')
+    itemEl.classList.add('gallery__item')
+    itemEl.setAttribute('href', original )
+
+    const imageEl = document.createElement('img')
+    imageEl.classList.add('gallery__image')
+    imageEl.setAttribute('src', preview)
+    imageEl.setAttribute('alt', description)
+
+    itemEl.append(imageEl)
+
+    return itemEl
+}
+
+const cards = galleryItems.map(makeCard)
+
+refs.galleryEl.append(...cards)
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
